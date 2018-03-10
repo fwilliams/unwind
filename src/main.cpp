6eb28676642741bd76ad9ed1e86576bfa986811e
Francis Williams <francis@fwilliams.info>
2018-03-10 16:52:53 -0500

    we can straighten a fish

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,8 @@
 #include <igl/bbw.h>
 #include <igl/normalize_row_sums.h>
 #include <igl/project_isometrically_to_plane.h>
+#include <igl/AABB.h>
+#include <igl/slim.h>
 
 #include <iostream>
 #include <utility>
@@ -125,10 +127,14 @@ void visualize_tet_wireframe(igl::opengl::glfw::Viewer& viewer,
 }
 
 
-void extract_skeleton(int num_samples, const Eigen::MatrixXd& TV, const Eigen::MatrixXi& TT, const Eigen::VectorXd isovals, Eigen::MatrixXd& SV) {
+void extract_skeleton(int num_samples, const Eigen::MatrixXd& TV, const Eigen::MatrixXi& TT,
+                      const Eigen::VectorXd isovals, Eigen::MatrixXd& SV, Eigen::VectorXi& sk_verts) {
+  using namespace std;
+
   SV.resize(num_samples, 3);
   Eigen::MatrixXd LV;
   Eigen::MatrixXi LF;
+  sk_verts.resize(num_samples);
   int vcount = 0;
   for(int i = 1; i < num_samples; i++) {
     double isovalue = i * (1.0/num_samples);
@@ -137,9 +143,21 @@ void extract_skeleton(int num_samples, const Eigen::MatrixXd& TV, const Eigen::M
       continue;
     }
     Eigen::RowVector3d C = LV.colwise().sum() / LV.rows();
-    SV.row(vcount++) = C;
+    int idx = -1;
+    double min_norm = std::numeric_limits<double>::infinity();
+    for (int k = 0; k < TV.rows(); k++) {
+      double norm = (TV.row(k) - C).norm();
+      if (norm < min_norm) {
+        idx = k;
+        min_norm = norm;
+      }
+    }
+    SV.row(vcount) = TV.row(idx);
+    sk_verts[vcount] = idx;
+    vcount += 1;
   }
 
+  sk_verts.conservativeResize(vcount);
   SV.conservativeResize(vcount, 3);
 }
 
@@ -154,7 +172,7 @@ void draw_skeleton(Viewer& viewer, const Eigen::MatrixXd& SV) {
 }
 
 bool bbw(const Eigen::MatrixXd& SV, const Eigen::MatrixXd& TV, const Eigen::MatrixXi& TT, Eigen::MatrixXd& W) {
- // Need to construct
+  using namespace std;
   Eigen::MatrixXi BE(SV.rows() - 1, 2);
 
   for (int i = 0; i < SV.rows()-1; i++) {
@@ -163,20 +181,49 @@ bool bbw(const Eigen::MatrixXd& SV, const Eigen::MatrixXd& TV, const Eigen::Matr
 
   Eigen::VectorXi b;
   Eigen::MatrixXd bc;
-  igl::boundary_conditions(TV, TT, SV, Eigen::VectorXi(), BE, Eigen::MatrixXi(), b, bc);
+  if (!igl::boundary_conditions(TV, TT, SV, Eigen::VectorXi(), BE, Eigen::MatrixXi(), b, bc)) {
+    cerr << "suspicious boundary conditions!" << endl;
+    cerr << bc.rows() << ", " << bc.cols() << endl;
+//    return false;
+  }
   igl::BBWData bbw_data;
-  //  bbw_data.active_set_params.max_iter = 3;
+  bbw_data.active_set_params.max_iter = 3;
   bbw_data.verbosity = 2;
   return igl::bbw(TV, TT, b, bc, bbw_data, W);
+}
+
+bool slim(const Eigen::MatrixXd& TV, const Eigen::MatrixXi& TT, Eigen::VectorXi& b, igl::SLIMData& sData, bool first) {
+  if (first) {
+    Eigen::MatrixXd bc(b.rows(), 3);
+    bc.row(0) = TV.row(b[0]);
+    for (int i = 0; i < b.rows()-1; i++) {
+      Eigen::RowVector3d s = TV.row(b[i]);
+      Eigen::RowVector3d n = TV.row(b[i+1]);
+      double d = (n - s).norm();
+      bc.row(i+1) = bc.row(i) + Eigen::RowVector3d(0.0, 0.0, d);
+    }
+
+    Eigen::MatrixXd TV_0 = TV;
+    double soft_const_p = 1e5;
+    sData.exp_factor = 5.0;
+    slim_precompute(TV, TT, TV_0, sData, igl::SLIMData::EXP_CONFORMAL, b, bc, soft_const_p);
+  } else {
+    slim_solve(sData, 20);
+  }
 }
 
 int main(int argc, char *argv[]) {
   using namespace Eigen;
   using namespace std;
 
+  if (argc > 1 && strning(argv[1]) == "--mesh") {
+    if (argc != 2) { cerr << "--mesh expected .dat file argument" << endl; }
+  }
   Eigen::MatrixXd TV, isoV, SV, W;
   Eigen::MatrixXi TF, TT, isoF;
   Eigen::VectorXd isovals;
+  Eigen::VectorXi sk_verts;
+  igl::SLIMData sData;
 
   igl::opengl::glfw::Viewer viewer;
   igl::opengl::glfw::imgui::ImGuiMenu menu;
@@ -188,12 +235,15 @@ int main(int argc, char *argv[]) {
   viewer.data().clear();
   viewer.data().set_mesh(TV, TF);
 
+  cout << "Input mesh has " << TV.rows() << " vertices and " << TT.rows() << " tets" << endl;
+
   array<int, 2> selected_end_coords{{-1, -1}};
   int current_idx = 0;
   bool done_harmonic = false;
   double level_set = 0.0;
   double increment = 0.05;
   int current_weight = 0;
+  bool slim_first_iter = true;
 
   viewer.callback_key_down = [&](igl::opengl::glfw::Viewer& viewer,
                                  unsigned char key, int modifiers) -> bool {
@@ -238,7 +288,7 @@ int main(int argc, char *argv[]) {
       if (isovals.rows() != TV.rows()) {
         cerr << "No isofunction defined yet!" << endl;
       } else {
-        extract_skeleton(25, TV, TT, isovals, SV);
+        extract_skeleton(25, TV, TT, isovals, SV, sk_verts);
         viewer.data().clear();
         viewer.data().set_mesh(TV, TF);
         viewer.data().show_faces = false;
@@ -257,6 +307,16 @@ int main(int argc, char *argv[]) {
       }
       cout << W.rows() << " x " << W.cols() << endl;
       break;
+    case 'X':
+      cout << "About to do slim" << endl;
+      slim(TV, TT, sk_verts, sData, slim_first_iter);
+      slim_first_iter = false;
+      cout << "Done slim" << endl;
+      viewer.data().clear();
+      viewer.data().set_mesh(TV, TF);
+      viewer.data().show_faces = false;
+      visualize_tet_wireframe(viewer, sData.V_o, TT, isovals);
+      draw_skeleton(viewer, sData.bc);
     case '.':
       if (W.cols() != 0) {
         current_weight = (current_weight + 1 % W.cols());
